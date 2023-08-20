d8ca79c5507899080ea981654b255ae0ee58de7d
priyankadeshavath <92449650+priyankadeshavath@users.noreply.github.com>
2023-08-20 21:25:19 +0530

    Add files via upload

---
+++ largestnumber.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class largestnumber {
+
+	public static void main(String[] args) {
+		int a=33,b=44,c=0;
+		if(a>b && a>c)
+			System.out.println(a + " is the largest number");
+		else if(b>a &&b>c)
+			System.out.println(b + " is the largest number");
+		else 
+			System.out.println(c + " c is the largest number");
+	}
+	
+}
+
